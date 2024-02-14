928145ced0d580adb24e7531a1d64819b10f2e64
nachoneca <8666@holbertonstudents.com>
2024-02-14 18:11:55 +0000

    commit

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - esta funcion es para escribir el comentario en puts
+ *Return: el valor de error de la funcion
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
