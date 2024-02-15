9171bbc65248a60712f7a7f50c606e704ab54b2c
nachoneca <8666@holbertonstudents.com>
2024-02-15 18:11:19 +0000

    commit

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x <= 'a'; x--)
+	putchar(x);
+	putchar('\n');
+
+	return (0);
+}
