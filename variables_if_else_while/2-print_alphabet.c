3a92debc482bbbca324f3b56b72993315549a3e3
nachoneca <8666@holbertonstudents.com>
2024-02-15 16:18:33 +0000

    commit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: The error return
+ */
+/* betty style doc for function main goes there */
+int main (void)
+{
+	for(char x= 'a'; x <= 'z'; x++)
+	{
+	putchar(x);
+	putchar('\n');
+	}
+	return (0);
+}
