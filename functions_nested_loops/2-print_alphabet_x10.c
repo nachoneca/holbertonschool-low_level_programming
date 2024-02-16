e9553f5e3a445f5e1cb9d429803afd7ddf5fa54a
nachoneca <8666@holbertonstudents.com~>
2024-02-16 16:55:06 +0000

    commit

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* Lo que quiero realizar es un loop de n veces del abecedario 
+* Haciendolo de esta manera utilizo la funcion de imprimir el abcedario 
+*/
+int main(void)
+{
+int abc
+for (abc = '0'; abc < '10'; abc++)
+{
+print_alphabet;
+_putchar(abc);
+}
+_putchar('\n');
+}
