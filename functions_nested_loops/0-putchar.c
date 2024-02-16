b90c243469cf1b2267fe82cef01d7ce5225cc89b
nachoneca <8666@holbertonstudents.com~>
2024-02-16 13:34:08 +0000

    commit

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+_putchar('_' && 'p' && 'u' && 't' && 'c' && 'h' && 'a' && 'r');
+_putchar('\n');
+return (0);
+}
