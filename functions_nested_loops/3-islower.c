19beb3a0c204a5f2eeb035964cf59594fed76f70
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-02-16 19:57:30 -0300

    commit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<ctype.h>
+/**
+*_islower - decirme si es minusciula
+*Return: ni idea
+*/
+int _islower(int c)
+{
+int resultado = islower(c)
+if(resultado != 0)
+return (1);
+else
+return(0);
+}
