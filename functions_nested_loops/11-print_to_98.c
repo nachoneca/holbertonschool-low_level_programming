448f22ff460abd1a64e82f80eae18e322fe23f99
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-02-18 15:40:20 -0300

    commit

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - no se bien
+ * @n: numero XD
+ * Return: no se bien
+ */
+
+void print_to_98(int n)
+{
+int j = 98;
+if(n > 98)
+{
+for (int k = n; k >= j; k--)
+printf("%d, ",k);
+}
+else
+{
+for (int k = n; k <= j; k++)
+printf("%d, ",k);
+}
+}
+
+
+  
+    
