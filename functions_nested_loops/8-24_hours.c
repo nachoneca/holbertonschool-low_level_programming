e0d5d812bc78d7785367507dc87e913c8b7ac173
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-02-18 14:22:57 -0300

    commit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *jack_bauer - imprime los minutos
+ *Return: void
+ */
+void jack_bauer(void)
+{
+  int hora, min;
+for (hora == 0; hora < 23; ++hora)
+{
+  for (min == 0; hora < 59; ++min)
+ {
+			printf("%02d:%02d\n", hora, minuto);
+		}
+	}
+}   
