0530a4ffffbb4ddcc0933248a5e6fa7c3749badd
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-02-18 14:26:33 -0300

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@
 void jack_bauer(void)
 {
   int hora, min;
-for (hora == 0; hora < 23; ++hora)
+for (hora == 0; hora <= 23; ++hora)
 {
-  for (min == 0; hora < 59; ++min)
+  for (min == 0; hora <= 59; ++min)
  {
 			printf("%02d:%02d\n", hora, minuto);
 		}
