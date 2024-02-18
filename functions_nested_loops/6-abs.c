a48efa1b2b884aa2b226bbab441b0e46ed1667d5
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-02-18 13:50:11 -0300

    commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*_isalpha - devuelve si es mayuscula o no
+*@c: parametro letras
+*Return: 0 o 1 segun lo que sea
+*/
+int _abs(int n)
+{
+int num = abs(n);
+return(num);
+}
