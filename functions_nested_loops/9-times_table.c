27c1c40686c203a78e9447964342d663391c2027
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-02-18 14:57:44 -0300

    commit

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *times_table - tabla del 0 al 9
+ *Return: nada
+ */
+
+void times_table(void)
+{
+int f; c;
+for (int f = 0; f < 10; f++) 
+{
+  for (int c = 0; c < 10; c++) 
+{
+_putchar (%2d; f * c)
+}
+_putchar('\n')
+}
+}
