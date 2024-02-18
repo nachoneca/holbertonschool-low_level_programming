b1547cd142ab506038b5bc7f61d16f2d2bbc0352
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-02-18 11:46:56 -0300

    commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*_isalpha - devuelve si es mayuscula o no
+*@c: parametro letras
+*Return: 0 o 1 segun lo que sea
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar("+");
+return (1);
+}
+else if (n == 0)
+{
+_putchar ("0");
+return (0);
+}
+else
+{
+_putchar("-");
+return (-1);
+}
+}
+  
