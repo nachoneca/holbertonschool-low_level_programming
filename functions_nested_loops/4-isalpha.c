9952ede6bcd7fafa783efed1fa85dae2cc023043
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-02-18 11:33:56 -0300

    commit

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,2 +1,18 @@
 #include "main.h"
 #include <ctype.h>
+/**
+*_isalpha - devuelve si es mayuscula o no
+*@c: parametro letras
+*Return: 0 o 1 segun lo que sea
+*/
+int _isalpha(int c)
+{
+  if (isalpha(c))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
