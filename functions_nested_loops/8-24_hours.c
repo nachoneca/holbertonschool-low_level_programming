3ce14d11134090734a5d1d4ce3227407c2790a7c
nachoneca <8666@holbertonstudents.com>
2024-02-19 13:28:25 +0000

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ int hora, min;
 for (hora = 0; hora <= 23; ++hora)
 {
 for (min = 0; min <= 59; ++min)
-_putchar((hora / 10) + '0')
-_putchar((hora % 10) + '0')
-_putchar(':')
-_putchar((min / 10) + '0')
-_putchar((min % 10) + '0')
+_putchar((hora / 10) + '0');
+_putchar((hora % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
 }
 }
 }
