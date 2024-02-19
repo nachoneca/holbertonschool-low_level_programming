6bd1e5256daf7c9efde3acdf07393a15d8b3578d
nachoneca <8666@holbertonstudents.com>
2024-02-19 12:47:33 +0000

    commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
 *print_last_digit - asdsa
-*@n: asdsa
+*@c: asdsa
 *Return: sada
 */
-print_last_digit(int c)
+int print_last_digit (int c)
 {
 int udig = c % 10;
 _putchar('0' + udig);
-return(udig);
+return (udig);
 }
