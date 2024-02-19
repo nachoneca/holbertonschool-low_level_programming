dca0d42b6fc3ed621c58388db83a70e16874ab45
nachoneca <8666@holbertonstudents.com>
2024-02-19 23:12:35 +0000

    commit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *  * _isdigit - devuede si es digito o no
- *   * @c: el in si es un digito
- *    * Return: retorna 0 o 1 si e sun numero o no
- *     */
+*_isdigit - devuede si es digito o no
+* @c: el in si es un digito
+* Return: retorna 0 o 1 si e sun numero o no
+*/
 
 int _isdigit(int c)
 {
-	if (isdigit(c)> 0)	
+	if (isdigit(c) > 0)	
 		return (1);
 	else
 		return (0);
