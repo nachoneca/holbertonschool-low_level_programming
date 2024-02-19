33ee14f117214758c6913010588c8a0ec8446766
nachoneca <8666@holbertonstudents.com>
2024-02-19 14:02:54 +0000

    commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,11 +15,6 @@ for (c = 0; c < 10; c++)
 int result = f * c;
 if (result < 10)
 _putchar(result + '0');
-if (c != 9)
-{
-_putchar(',');
-_putchar(' ');
-}
 else
 {
 _putchar((result / 10) + '0');
