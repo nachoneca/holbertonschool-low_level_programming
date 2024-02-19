272a5646d09943e02002389a51acc9f0a17daf11
nachoneca <8666@holbertonstudents.com>
2024-02-19 13:44:29 +0000

    commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,20 +7,20 @@
 
 void times_table(void)
 {
-int f; c;
-for (int f = 0; f < 10; f++) 
+int f, c;
+for (f = 0; f < 10; f++)
 {
-for (int c = 0; c < 10; c++)
+for (c = 0; c < 10; c++)
 {
 int result = f * c;
-if(result < 0)
-_printf(result + '0')
+if (result < 0)
+_printf(result + '0');
 else
 {
-_putchar((result / 10) + '0')
-_putchar((result % 10) + '0')	
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
 }
-_putchar('\n')
+_putchar('\n');
 }
 }
 }
