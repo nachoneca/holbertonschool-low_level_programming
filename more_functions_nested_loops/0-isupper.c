35e0b0ab6b7199965acbbae5450ce1d84d1dec2d
nachoneca <8666@holbertonstudents.com>
2024-02-19 23:04:09 +0000

    commit

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- *  *_isupper - es o no es mayuscula
- *   *@c: numero
- *    *Return: es o no es mayuscula
- *     */
+ *_isupper - es o no es mayuscula
+ *@c: numero
+ *Return: es o no es mayuscula
+ */
 
 int _isupper(int c)
 {
-	if(isupper(c) > 0)
+	if (isupper(c) > 0)
 		return (1);
 	else
-	 	return (0);
+		return (0);
 }
