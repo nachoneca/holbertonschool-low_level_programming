a16a6f3572d420fa6bfdce8c22284a7479bbfb78
nachoneca <8666@holbertonstudents.com>
2024-02-19 16:10:02 +0000

    commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -23,10 +23,10 @@ void times_table(void)
 			if (res <= 9)
 				_putchar(' ');
 			else
-			{
+			
 				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
-			}
+				
+			_putchar((res % 10) + '0');}
 		}
 		_putchar('\n');
 	}
