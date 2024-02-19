5925c6677087bf8541774e4e8f2e8bc8fc654e71
nachoneca <8666@holbertonstudents.com>
2024-02-19 12:58:07 +0000

    commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,12 @@
 *@c: asdsa
 *Return: sada
 */
-int print_last_digit (int c)
+int print_last_digit(int c)
 {
 int udig = c % 10;
 if (udig < 0)
-_putchar('0' + (udig * -1));
+printf("%d", (udig * -1));
 else
-_putchar('0' + udig);
+printf("%d", udig);
 return (udig);
 }
