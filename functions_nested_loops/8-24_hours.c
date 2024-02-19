6a950e9349e5c8c8468b930d3b456a3bd8e38f7d
nachoneca <8666@holbertonstudents.com>
2024-02-19 13:14:59 +0000

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 int hora, min;
 for (hora = 0; hora <= 23; ++hora)
 {
-for (min = 0; hora <= 59; ++min)
+for (min = 0; min <= 59; ++min)
 printf("%02d:%02d\n", hora, min);
 }
 }
