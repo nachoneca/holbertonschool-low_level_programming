548f565f693dc84dee0ce9b5e83f187f1ad8c3a7
nachoneca <8666@holbertonstudents.com>
2024-02-19 23:29:46 +0000

    commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplica dos numeros
+ * @a: numero uno
+ * @b: numero dos
+ * Return: retorna la multi
+ */
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}
