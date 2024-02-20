0d1c3850e972836a154d998cbc4c11e4d5192fc7
nachoneca <8666@holbertonstudents.com>
2024-02-20 15:00:54 +0000

    commit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,17 @@ int main(void)
 	for (c = 1; c <= 100; c++)
 	{
 		if (c % 3 == 0 && c % 5 == 0)			
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (c % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (c % 3 == 0)
-			printf("Fizz ");
-		else if (c == 100)
-			printf("Buzz\n");
-		else
-			printf("%d ", c);
+			printf("Fizz");
+		else if
+			printf("%d", c);
+		
+		if (c != 100)
+			printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
