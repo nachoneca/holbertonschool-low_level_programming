9e528750cbc47844693fdd03490438e67b078cce
nachoneca <8666@holbertonstudents.com>
2024-02-20 13:46:08 +0000

    commit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - imprime un cuadrado
+ * @size: lol
+ * Return: nada
+ */
+void print_square(int size)
+{
+	int n, j;
+
+	n = size;
+	j = size;
+	for (j = 0; j < size; j++)
+	{
+		for (n = 0; n < size; n++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
