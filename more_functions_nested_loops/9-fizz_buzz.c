47c06817dc8d1c3db6047218cf790caf1bda010a
nachoneca <8666@holbertonstudents.com>
2024-02-20 14:39:43 +0000

    commit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "stdio.h"
+/**
+*main - imprime del 1 al 100
+*
+* Return: 0
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 100; c++)
+	{
+		if (c % 3 == 0)
+			printf("Fizz ");
+		else if (c % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", c);
+	}
+	return (0);
+}
