e47ce7e6c8ff6ad024c1205cf0e93e84a48e7112
nachoneca <8666@holbertonstudents.com>
2024-02-20 17:46:10 +0000

    commit

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - draw a diagonal line
+ * @size: number of times the '#' char is printed
+ * 
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+    for (i = 1; i <= size; i++)
+    {
+	for (j = 1; j <= size - i; j++)
+	{
+		_putchar(' ');
+	}
+	for (j = 1; j <= i; j++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+    }
+
+	if (size < 1)
+	{
+		_putchar('\n');
+	}
+}
