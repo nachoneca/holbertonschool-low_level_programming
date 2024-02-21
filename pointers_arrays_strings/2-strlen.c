02782c12f2975f2a5ba176edc7607f9c501ecfdb
nachoneca <8666@holbertonstudents.com>
2024-02-21 16:48:04 +0000

    commit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_strlen - prnt size of string
+ *@s: name of pointer
+ *Return: command of size of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#include <string.h>
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
+
+int _strlen(char *s);
