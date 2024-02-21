af5ff2cdaaf3664cbeb5504c6760d0540b088dd0
nachoneca <8666@holbertonstudents.com>
2024-02-21 16:15:55 +0000

    commit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *reset_to_98 - is to change te value of a variable
+ *@n: name of pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+On this repository im going to start with:
+*Pointers
+*Arrays
+*Strings
+
