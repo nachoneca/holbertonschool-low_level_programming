1c9a1ae89bce13cb425e7b059cde71032dd87275
nachoneca <8666@holbertonstudents.com>
2024-02-22 17:47:28 +0000

    commit

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
     char *str;
 
-    str = "0123456789";
+    str = "0 1234 5 67 8 9";
     puts_half(str);
     return (0);
 }
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,9 +11,6 @@ void puts_half(char *str)
 	for (a = 0; str[a] != '\0'; a++)
 		;
 	b = a / 2;
-	if (a % 2 != 0)
-		_putchar(str[b]-(b-1));
-	else
 		for (; str[b] != '\0'; b++)
 			_putchar(str[b]);
 	_putchar('\n');
