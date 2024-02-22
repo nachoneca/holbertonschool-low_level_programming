45e893036ca49b58dfdbe15da6b8b4401bf4e1e4
nachoneca <8666@holbertonstudents.com>
2024-02-22 23:38:36 +0000

    commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_array - prints thngs inside array
+*@a: name array
+*@n: number of data given
+*/
+void print_array(int *a, int n)
+{
+	int size;
+
+	for (size = 0; size < n; size++)
+	{
+		printf("%d", a[size]);
+			if (size != '\0')
+				printf(", ");
+	}
+
+}
