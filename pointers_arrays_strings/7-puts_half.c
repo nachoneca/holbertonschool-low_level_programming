068584738b5f4a6078bc2bad621d51a3e49f7091
nachoneca <8666@holbertonstudents.com>
2024-02-22 16:48:19 +0000

    commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ *
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int a, b;
+
+	for(a = 0; str[a] != '\0'; a++)
+		;
+	b = a / 2;
+	for(; b <= a; b++)
+		_putchar(str[b]);
+	_putchar('\n');
+
+}
