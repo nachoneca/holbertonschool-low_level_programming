6af48314275ac99e64e665568d17a8210ca2f3d6
nachoneca <8666@holbertonstudents.com>
2024-02-22 17:18:56 +0000

    commit

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,20 @@
 #include"main.h"
 /**
- *
- *
+ *puts_half - to print half of str
+ *@str: name of str
  *
  */
 void puts_half(char *str)
 {
 	int a, b;
 
-	for(a = 0; str[a] != '\0'; a++)
+	for (a = 0; str[a] != '\0'; a++)
 		;
 	b = a / 2;
 	if (a % 2 != 0)
-                        _putchar(b - 1);
+		_putchar(b - 1);
 	else
-		for(; b <= a; b++)
+		for (; str[b] != '\0'; b++)
 			_putchar(str[b]);
+	_putchar('\n');
 }
