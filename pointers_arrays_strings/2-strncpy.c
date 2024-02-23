aed504127d7c2f45044230d47a79b5766090889e
nachoneca <8666@holbertonstudents.com>
2024-02-23 17:40:11 +0000

    commit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ *_strncpy - copy one tring to another until n characters
+ *@dest: string to paste main string
+ *@src: main string
+ *@n: numer of char that want to be copied
+ *Return: string copied
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+	return (dest);
+}
