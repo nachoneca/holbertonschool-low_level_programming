9cdf3018069f0ebced29f66fe8c069830d79731b
nachoneca <8666@holbertonstudents.com>
2024-02-23 12:45:15 +0000

    commit

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ *_atoi - takes a string and make it a number
+ *@s: the string given to us
+ *Return: number * sign
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int num = 0;
+
+	for (; *s != '\0'; s++)
+	{
+	if (*s == '-')
+		sign *= -1;
+	else if (*s >= '0' && *s <= '9')
+		num = num * 10 + (*s - '0');
+	else if (num > 0)
+	break;
+	}
+	return (num * sign);
+}
