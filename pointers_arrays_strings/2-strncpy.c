052deab4af67ec27f7fd110847b23edbe1ca68c5
nachoneca <8666@holbertonstudents.com>
2024-02-23 17:51:39 +0000

    commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (a = 0; a < n && src[a] != '\0'; a++)
 		dest[a] = src[a];
-	for(; a < n; a++)
+	for (; a < n; a++)
 		dest[a] = '\0';
 	return (dest);
 }
