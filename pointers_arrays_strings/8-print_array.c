68b49f41ebf5220880341ff91be4ad6f39bd6638
nachoneca <8666@holbertonstudents.com>
2024-02-23 00:00:32 +0000

    commit

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,8 +11,10 @@ void print_array(int *a, int n)
 	for (size = 0; size < n; size++)
 	{
 		printf("%d", a[size]);
-			if (size != '\0')
-				printf(", ");
+		if (size != n - 1)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
