9d78a8156450f1cd47aadd67085b4334b7b98e81
nachoneca <8666@holbertonstudents.com>
2024-02-24 14:47:08 +0000

    commit

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ *reverse_array - is to reverse an array of num
+ *@a: is the array we want to reverse
+ *@n: is the size of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, b;
+
+	for (; a[i] < n / 2; i++)
+	{
+		b = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = b;
+	}
+}
