4ef96613c00842f3c084a3d198501a560953711b
nachoneca <8666@holbertonstudents.com>
2024-02-24 15:22:29 +0000

    commit

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  *string_toupper - is to change minus into mayus in str
- *str: name of string
+ *@str: name of string
  *Return: return str
  */
 char *string_toupper(char *str)
