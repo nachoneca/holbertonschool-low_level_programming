5f26bd06a0d573c80449d1b1aea9154120955d84
nachoneca <8666@holbertonstudents.com>
2024-02-24 15:50:21 +0000

    commit

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ *cap_string - cap all words of str
+ *@str: name of str
+ *Return: string
+ */
+char* cap_string(char* str) 
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			putchar(toupper(str[i]));
+		}
+		else
+		{
+			putchar(str[i]);
+		}
+		for (; (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'); i++)
+		{
+			putchar(str[i]);
+		}
+		putchar(' ');
+	}
+	return (str);
+}
