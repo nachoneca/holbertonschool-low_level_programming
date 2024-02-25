b0f784af381ad32904fa161705bf425756e25642
nachoneca <8666@holbertonstudents.com>
2024-02-25 19:16:49 +0000

    commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,10 +6,19 @@
  */
 char *cap_string(char *str)
 {
-	int i, j;
+	int i, j, first_word = 1;
 
 	for (i = 0; str[i]; i++)
 	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		{
+			first_word = 1;
+		}
+		if (first_word && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+			first_word = 0;
+		}
 		if (str[i] < 'A' || (str[i] > 'Z' && str[i] < 'a') || str[i] > 'z')
 		{
 			putchar(str[i]);
