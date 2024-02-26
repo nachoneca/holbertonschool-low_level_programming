b9014367654f49fc070c4f0ea43ef8c829f05191
nachoneca <8666@holbertonstudents.com>
2024-02-26 16:15:37 +0000

    commit

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,11 +7,12 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int a, b, coin = 0;
+	int a, b, coin;
 	unsigned int c = 0;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
+		coin = 0;
 		for (b = 0; accept[b] != '\0'; b++)
 		{
 			if (s[a] == accept[b])
@@ -21,8 +22,8 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 		}
-	if (!coin)
-		break;
+		if (!coin)
+			break;
 	}
 	return (c);
 }
