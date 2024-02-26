3f961f045102cbca64000518342ce23318294025
nachoneca <8666@holbertonstudents.com>
2024-02-26 18:19:34 +0000

    commit

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -3,22 +3,21 @@
  *_strstr - locate a string inside anotehr
  *@haystack: main string
  *@needle: tring to locate inside main
- *Resturn: the position of the amin str when second str matches
+ *Return: the position of the amin str when second str matches
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int a, b;
+	int a, b;
 
 	for (a = 0; haystack[a] != '\0'; a++)
 	{
 		for (b = 0; needle[b] != '\0' && haystack[a + b] == needle[b]; b++)
 		{
 			;
-     		}
-
+		}
 		if (needle[b] == '\0')
 			return (haystack + a);
-		}
-		return (NULL);
+	}
+	return (NULL);
 }
 
