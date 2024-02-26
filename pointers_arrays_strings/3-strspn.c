10a3b9b1bfe311f5c997a740a17396f6295ca602
nachoneca <8666@holbertonstudents.com>
2024-02-26 15:22:46 +0000

    commit

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ *_strspn - give us n of prefix substring
+ *@s: main str
+ *@accept: str to check
+ *Return: num of char repeated
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b, coin = 0;
+	unsigned int c = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				c++;
+				coin = 1;
+			}
+		}
+		if (!coin)
+			break;
+	}
+	return (c);
+}
