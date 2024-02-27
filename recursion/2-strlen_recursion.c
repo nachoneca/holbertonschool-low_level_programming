7070408d688b22e24d1ef402ff8034df8bedd849
nachoneca <8666@holbertonstudents.com>
2024-02-27 14:29:22 +0000

    commit

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ *_strlen_recursion - return the length of the str
+ *@s: name of pointer to str
+ *Return: value of a
+ */
+int _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (s[a] == '\0')
+	{
+		return (0);
+	}
+	return(1 + _strlen_recursion(s + 1));
+}
