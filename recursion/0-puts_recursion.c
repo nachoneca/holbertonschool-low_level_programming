9ddd2ce317e26e943664dbbbc81ecd195e823c08
nachoneca <8666@holbertonstudents.com>
2024-02-27 13:12:14 +0000

    commit

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,15 @@
 #include"main.h"
 /**
- *
- *
- *
- *
+ *_puts_recursion - print a string with recursion
+ *@s: name of pointer to string
  */
 void _puts_recursion(char *s)
 {
 	int a = 0;
-	
-	
+
 	if (s[a] == '\0')
-	{	_putchar('\n');
+	{
+		_putchar('\n');
 		return;
 	}
 	_putchar(s[a]);
