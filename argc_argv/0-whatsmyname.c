ea8ea8041d875c0fe2f3d19f08c7e0035f543e98
nachoneca <8666@holbertonstudents.com>
2024-02-28 13:16:11 +0000

    commit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include"main.h"
 /**
- *my_name_is - prints name of program
+ *main - prints name of program
  *@argc: variable that will flollow th n of arguments
  *@argv: contains the name of the program
  *Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc <= 1)
 		printf("%s\n", argv[0]);
