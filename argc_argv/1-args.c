9c77e42bc29693a3de5d1ae1d1f5543a04cb28e5
nachoneca <8666@holbertonstudents.com>
2024-02-28 13:47:39 +0000

    commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ *main - prints num of arguments
+ *@argc: count the arguments
+ *@argv: call the string inside
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc);
+	return (0);
+}
