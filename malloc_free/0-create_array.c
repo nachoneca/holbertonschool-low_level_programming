8c798b2abd54a9dabe33a9464085606b2e2a68e2
nachoneca <8666@holbertonstudents.com>
2024-02-28 17:47:14 +0000

    commit

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ *create_array - creates an array of char
+ *
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	
+
+
+}	
---
+++ malloc_free/README.md
@@ -0,0 +1,2 @@
+On this directory we are going to use MALLOC and FREE
+
