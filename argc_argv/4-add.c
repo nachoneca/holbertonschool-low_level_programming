5a339adbf16c3e05a7ed8a0ad62b3c2598f17536
nachoneca <8666@holbertonstudents.com>
2024-02-28 17:15:22 +0000

    commit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,15 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = 0, res = 0;
+	int a = 0, b = 0, res = 0;
 
 	for (a = 1; a < argc; a++)
 	{
-		if (argv[a][0] < 48 || argv[a][0] > 57)
-		{
-			printf("Error\n");
-			return (1);
-		}
+		for (; argv[a][b]; b++)
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		res += atoi(argv[a]);
 	}
 	printf("%d\n", res);
