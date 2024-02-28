55a219dc6664a15675a414ffead47e141623dc8b
nachoneca <8666@holbertonstudents.com>
2024-02-28 15:10:13 +0000

    commit

---
+++ argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <ctype.h>
+#include"main.h"
+/**
+ *main - adds all numbers of the arguments
+ *@argc: number of arguments
+ *@argv: inside of each arg
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, res = 0;
+
+	if (argc == 0)
+		printf("0\n");
+	for (; argv[a]; a++)
+	{
+		if (!isdigit(argv[a]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		res = res + a;
+	}
+	printf("%i\n", res);
+	return (0);
+}
