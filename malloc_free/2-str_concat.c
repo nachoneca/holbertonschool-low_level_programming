6e28a5e2b602272b2ffd921d76cbd6dae1f835ee
nachoneca <8666@holbertonstudents.com>
2024-02-29 17:29:24 +0000

    commit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,20 +14,20 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (l1 = 0; s1[l1] != '\0'; l1++)
+	for (l1 = 0; s1[l1] != '\0'; l1++);
 	
-	for (l2 = 0; s2[l2] != '\0'; l2++)
+	for (l2 = 0; s2[l2] != '\0'; l2++);
 
-	unidos = malloc((l1 + l2) * sizeof(char));
+	unidos = malloc((l1 + l2 + 1) * sizeof(char));
 	if (unidos == NULL)
 		return (NULL);
 	for (l1 = 0; s1[l1] != '\0'; l1++)
-	{
 		unidos[a++] = s1[l1];
-	}
 	for (l2 = 0; s2[l2] != '\0'; l2++)
 	{
-		unidos[a++] = s2[l2];
+		unidos[a] = s2[l2];
+		a++;
+
 	}
 	return (unidos);
 }
