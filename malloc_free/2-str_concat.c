f0f21b8e63c5eb983db637f428bcaeae881f42cd
nachoneca <8666@holbertonstudents.com>
2024-02-29 17:32:47 +0000

    commit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,23 +1,23 @@
 #include"main.h"
 /**
- *
- *
- *
- *
+ *str_concat - concat two strings
+ *@s1: first str
+ *@s2: scnd str
+ *Return: strngs concanated
  */
 char *str_concat(char *s1, char *s2)
 {
- 	int l1, l2, a = 0;
+	int l1, l2, a = 0;
 	char *unidos;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (l1 = 0; s1[l1] != '\0'; l1++);
-	
-	for (l2 = 0; s2[l2] != '\0'; l2++);
-
+	for (l1 = 0; s1[l1] != '\0'; l1++)
+		;
+	for (l2 = 0; s2[l2] != '\0'; l2++)
+		;
 	unidos = malloc((l1 + l2 + 1) * sizeof(char));
 	if (unidos == NULL)
 		return (NULL);
