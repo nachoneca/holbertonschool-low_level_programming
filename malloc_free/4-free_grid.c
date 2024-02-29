0686c010cedc513adeaaf3e2ddd5463956ebb8de
nachoneca <8666@holbertonstudents.com>
2024-02-29 23:48:58 +0000

    commit

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,11 +2,12 @@
 /**
  *free_grid - frees all memory of array
  *@grid: pointer to my bidim array
- *@hight: y of my bidim array
+ *@height: y of my bidim array
  */
 void free_grid(int **grid, int height)
 {
 	int b;
+
 	for (b = 0; b < height; b++)
 		free(grid[b]);
 	free(grid);
