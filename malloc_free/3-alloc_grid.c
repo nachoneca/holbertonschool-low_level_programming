f4ffaf6c1916dbe432bfbc1a7de27fee891b91d1
nachoneca <8666@holbertonstudents.com>
2024-02-29 23:31:08 +0000

    commit

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/**
+ *alloc_grid - allocates my grid with x and y
+ *@width: num of x axys
+ *@height: num of y axys
+ *Return: my mtz
+ */
+int **alloc_grid(int width, int height)
+{
+	int a, b, **mtz;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	mtz = (int **)malloc(height * sizeof(int *));
+	if (mtz == NULL)
+		return (NULL);
+	for (a = 0; a < height; a++)
+	{
+		mtz[a] = (int *)malloc(width * sizeof(int));
+		if (mtz[a] == NULL)
+		{
+			for (b = 0; b < a; b++)
+				free(mtz[b]);
+			free(mtz);
+			return (NULL);
+		}
+		for (b = 0; b < width; b++)
+			mtz[b] = 0;
+	}
+	return (mtz);
+}
