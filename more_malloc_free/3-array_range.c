bc65d1a889749f6c63135b1d1cfccc04ffbe24f4
nachoneca <8666@holbertonstudents.com>
2024-03-01 15:15:33 +0000

    commit

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ *array_range - create an array of range min to max
+ *@min: num min
+ *@max: num max
+ *Return: ptr
+ */
+int *array_range(int min, int max)
+{
+	int a, b;
+	int *ptr;
+	
+	if (min > max)
+		return (NULL);
+
+	for (a = min; a <= max; a++)
+		;
+	ptr = malloc(a *sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+	for (b = 0; b < a; b++)
+		ptr[b] = min + b;
+		
+	return (ptr);
+
+
+}
