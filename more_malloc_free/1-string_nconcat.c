3da0672173c409366e06fa0f162fb27d7b91ea37
nachoneca <8666@holbertonstudents.com>
2024-03-01 13:40:51 +0000

    commit

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include"main.h"
+/**
+ *string_nconcat - conctenate s1 and s2 until n
+ *@s1: first str
+ *@s2: second str
+ *@n: until what byte to concat s2
+ *Return: the str united
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int a, b, c, unidos;
+	char *res;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+	if (n > b)
+		n = b;
+	unidos = a + n;
+
+	res = malloc((unidos + 1) * sizeof(char));
+		if (res == NULL)
+			return (NULL);
+	for (a = 0; s1[a] != '\0'; a++)
+		res[c++] = s1[a];
+
+	for (b = 0; b <= n; b++)
+		res[c++] = s2[b];
+
+	return (res);
+}
