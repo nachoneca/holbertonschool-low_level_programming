ddf691d6cd5663ef2fe9f24cc2aa4385a8053720
nachoneca <8666@holbertonstudents.com>
2024-03-02 20:00:42 +0000

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,30 +11,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	int a = 0, b = 0, g;
 	dog_t *ptrnd;
+	
+	for (; name[a] != '\0'; a++);
+
+	for (; owner[b] != '\0'; b++);
 
 	ptrnd = malloc(sizeof(dog_t));
 	if (ptrnd == NULL)
 		return (NULL);
 
-	for (; name[a] != '\0'; a++)
-		;
-	for (; owner[b] != '\0'; b++)
-		;
-
-	ptrnd->name = malloc((a + 1) * sizeof(char));
+	ptrnd->name = malloc((a + 1) * sizeof(ptrnd->name));
 	if (ptrnd->name == NULL)
 		return (NULL);
 
 	for (g = 0; g < a; g++)
 		ptrnd->name[g] = name[g];
+	
+	ptrnd->age = age;
 
-	ptrnd->owner = malloc((b + 1) * sizeof(char));
+	ptrnd->owner = malloc((b + 1) * sizeof(ptrnd->owner));
 	if (ptrnd->owner == NULL)
 		return (NULL);
 
 	for (g = 0; g < b; g++)
 		ptrnd->owner[g] = owner[g];
 
-	ptrnd->age = age;
 	return (ptrnd);
 }
