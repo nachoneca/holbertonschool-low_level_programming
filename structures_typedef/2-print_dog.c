2017987b67e4cde2d37a02f958a6be94acbd56db
nachoneca <8666@holbertonstudents.com>
2024-03-02 16:49:42 +0000

    commit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,20 +8,19 @@
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
+	{
 		if (d->name == NULL)
 			printf("Name: (nil)");
+
 		else
 			printf("Name: %s\n", d->name);
-		if (d->age == NULL)
-			printf("(nil)");
-		else
-			printf("Age: %d\n", d->age);
+
+		printf("Age: %f\n", d->age);
+
 		if (d->owner == NULL)
 			printf("nil");
+
 		else
 			printf("Owner: %s\n", d->owner);
-
-
-
-
+	}
 }
