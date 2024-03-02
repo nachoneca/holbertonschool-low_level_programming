0332b33bca8f1ca8937f9c330c9dafe8309917b8
nachoneca <8666@holbertonstudents.com>
2024-03-02 16:00:43 +0000

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,18 @@
-
-
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structure representing a dog
+ *  * @name: the name of the dog (char *)
+ *   * @age: the age of the dog (float)
+ *    * @owner: the owner of the dog (char *)
+ *Description: structure that represents a dogs, name, age and owner
+ */
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+
+
+#endif
