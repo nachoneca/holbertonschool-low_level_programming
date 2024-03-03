c419d2fad09591c57d02ac557bcc20d65acf1cc7
nachoneca <8666@holbertonstudents.com>
2024-03-03 01:09:49 +0000

    commit

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include"function_pointers.h"
+/**
+ *int_index - function that is given ana array of size.
+ *that will complete the action of the function cmp
+ *@array: array of int
+ *@size: size of the array
+ *cmp: function that compares elements in the array and give t or f
+ *
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
