6f38e15123bd941d3721eed3dea4d9e7137f0852
nachoneca <8666@holbertonstudents.com>
2024-03-03 02:03:12 +0000

    commit

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+int op_mof(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
