e6594212724bd569fc260a1fe82cd1d4142c4dc2
nachoneca <8666@holbertonstudents.com>
2024-03-26 18:24:30 +0000

    first try

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@ size_t print_list(const list_t *h)
 	int count;
 
 	for (count = 0; h != NULL; h = h->next)
-	{	
-		if(h->str == NULL)
+	{
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 			count++;
 		}
 		else
-		{	
+		{
 			printf("[%i] %s\n", h->len, h->str);
 			count++;
 		}
