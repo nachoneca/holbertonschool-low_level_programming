02d7d01fb01fe215da630f8b550b6cea21ef0e8a
nachoneca <8666@holbertonstudents.com>
2024-03-27 23:37:08 +0000

    first try on 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+
+#include"lists.h"
+/**
+ *free_list - free the node
+ *@head: pointer to the stratr of the nodes chain
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
