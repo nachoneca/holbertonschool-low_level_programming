da3a19fdbc7e8dabb704a008b3baefeacf1351bf
nachoneca <8666@holbertonstudents.com>
2024-03-28 16:06:52 +0000

    First try on 4-free_dlistint.c

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include"lists.h"
+/**
+ *free_dlistint - frees all the node of the chain
+ *@head: pointer to the start of the chain of nodes
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{	
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
