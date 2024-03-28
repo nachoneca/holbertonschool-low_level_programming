3ce8643822e9667ca6729ddfd61361c1207da124
nachoneca <8666@holbertonstudents.com>
2024-03-28 00:05:53 +0000

    betty correction on on 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,17 @@
 #include"lists.h"
 /**
- *
- *
- *
+ *print_dlistint - prints every info inide each node
+ *@h: pointer to the start of the chain of nodes
+ *Return: returns the amount of nodes printed
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
-	
+
 	for (; h != NULL; h = h->next)
 	{
 		printf("%i\n", h->n);
 		count++;
 	}
 	return (count);
-	
 }
