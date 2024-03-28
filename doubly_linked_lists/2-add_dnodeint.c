a30bd80244a435144ef9a1a3ff59c7673a8f76b4
nachoneca <8666@holbertonstudents.com>
2024-03-28 00:36:59 +0000

    Second try on 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = *head;
+	new_node->prev = NULL;
 	*head = new_node;
 	return (new_node);
 }
