c2ee98514a060b328364f544495a5e12f10d79e3
nachoneca <8666@holbertonstudents.com>
2024-03-28 20:00:21 +0000

    Third try on 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	aux = *h;
-	if (aux->next == NULL)
+	if (aux == NULL)
 		return (add_dnodeint_end(h, n));
 	while (aux != NULL && i < idx - 1)
 	{	
